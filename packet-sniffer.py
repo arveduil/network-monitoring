efbdcb427e6fe0537b34845befd9403683ce2ef3
Piotr Boszczyk <pboszczyk@gmail.com>
2021-04-13 20:38:04 +0200

    add logging to SQLite

--- packet-sniffer.py
+++ packet-sniffer.py
@@ -1,55 +1,68 @@
 #! /usr/local/bin/python3.5
 
 import socket
-import struct
-import textwrap
 import binascii
 import struct
 import sys
 import os
+import datetime
+import sqlite3
 
 
+def get_now():
+    return datetime.datetime.now()
 
-def main():
+
+def create_insert_sql_query(timestamp: datetime, src: str, dest: str, size: int):
+    return str(f"""INSERT INTO TrafficLogs VALUES ('{timestamp}', '{src}', '{dest}', {str(size)})  """)
+
+
+def main( db_connection):
     conn = socket.socket(socket.PF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
     connections = {}
-    rdns = {}
     packet_count = 0
+    cur = db_connection.cursor()
 
+    cur.execute('''CREATE TABLE if not exists TrafficLogs 
+                   (timestamp datetime, source text, destination text, data integer)''')
+    db_connection.commit()
+
+    last_insert_timestamp_seconds = -1
     while True:
+        now_seconds =get_now().second
+        if now_seconds != last_insert_timestamp_seconds and now_seconds % 5 == 0:
+            last_insert_timestamp_seconds = now_seconds
+            for connection in connections:
+                inesrt_transfer_log(connection, connections[connection], cur)
+
+            connections = {}
+            db_connection.commit()
+
         packet_count = packet_count + 1
         raw_data, addr = conn.recvfrom(65536)
         dest_mac, src_mac, eth_proto, data = ethernet_frame(raw_data)
         data_size = len(raw_data)
         if eth_proto == 'IPV4':
-            srcDest = (src, dest) = getSrcDest(data, raw_data)
+            srcDest = get_source_destination(data, raw_data)
         else:
-            continue #srcDest = (src, dest) = ipv6Header(data)
-        if not src in rdns:
-        	rdns[src] = socket.getnameinfo((src, 0), 0)
-        if not dest in rdns:	
-        	rdns[dest] = socket.getnameinfo((dest, 0), 0)
+            # TODO handle IPv6 and other
+            continue
         if srcDest in connections:
             connections[srcDest] = connections[srcDest] + data_size
         else:
             connections[srcDest] = data_size
-        if packet_count == 100:
-        	clear = lambda: os.system('clear')
-        	clear()
-        	packet_count = 0
-        	for connection in connections:
-            		connection_transfer = connections[connection]
-            		print(str(connection) + '\t' + str(connection_transfer) + ' B' + '\t' + '{0:.0f}'.format(
-                	connection_transfer / 1024) + ' KB' + '\t' + '{0:.0f}'.format(connection_transfer / 1024 / 1024) + ' MB' + '\t' + str(
-                	rdns[connection[0]]) + ' -> ' + str(rdns[connection[1]]))
-        
+    db_connection.close()
 
 
+def inesrt_transfer_log(connection, connection_transfer, cur):
+    insert_query = create_insert_sql_query(get_now(), connection[0], connection[1], connection_transfer)
+    print(insert_query)
+    cur.execute(insert_query)
 
-def getSrcDest(data, raw_data):
+
+def get_source_destination(data, raw_data):
     (version, header_length, ttl, proto, src, target, data) = ipv4_Packet(data)
     return (src, target)
-
 
 
 def ethernet_frame(data):
@@ -82,10 +95,20 @@ def ipv4_Packet(data):
 def ipv4(addr):
     return '.'.join(map(str, addr))
 
+
 def ipv6Header(data):
     ipv6_src_ip = socket.inet_ntop(socket.AF_INET6, data[8:24])
     ipv6_dst_ip = socket.inet_ntop(socket.AF_INET6, data[24:40])
-    return (ipv6_src_ip, ipv6_dst_ip )
+    return (ipv6_src_ip, ipv6_dst_ip)
 
 
-main()
+try:
+    db_connection = sqlite3.connect('traffic.sqlite')
+    main( db_connection)
+except KeyboardInterrupt:
+    print('INTERUPTED !!!')
+    db_connection.close()
+    try:
+        sys.exit(0)
+    except SystemExit:
+        os._exit(0)
